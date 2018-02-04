23df14d4ee9db1a4c87ef273740bdeb32c976a22
Cameron <cameron.taylor.ninja@gmail.com>
2018-02-04 12:53:32 -0800

    copy pasted shitton of Map loading code

---
+++ source/TiledLevel.hx
@@ -0,0 +1,272 @@
+package;
+
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledMap.FlxTiledMapAsset;
+
+/**
+ * ...
+ * @author ninjaMuffin
+ */
+class TiledLevel extends TiledMap 
+{
+
+	// For each "Tile Layer" in the map, you must define a "tileset" property which contains the name of a tile sheet image 
+	// used to draw tiles in that layer (without file extension). The image file must be located in the directory specified bellow.
+	private inline static var c_PATH_LEVEL_TILESHEETS = "assets/data/";
+	
+	// Array of tilemaps used for collision
+	public var foregroundTiles:FlxGroup;
+	public var foregroundObjects:FlxGroup;
+	public var BGObjects:FlxGroup;
+	public var objectsLayer:FlxGroup;
+	public var backgroundLayer:FlxGroup;
+	public var collidableTileLayers:Array<FlxTilemap>;
+	
+	// Sprites of images layers
+	public var imagesLayer:FlxGroup;
+	
+	public function new(tiledLevel:Dynamic, state:PlayState)
+	{
+		super(tiledLevel);
+		
+		FlxG.log.add("CheckPoint1");
+		
+		imagesLayer = new FlxGroup();
+		foregroundTiles = new FlxGroup();
+		foregroundObjects = new FlxGroup();
+		BGObjects = new FlxGroup();
+		objectsLayer = new FlxGroup();
+		backgroundLayer = new FlxGroup();
+		
+		FlxG.camera.setScrollBoundsRect(0, 0, fullWidth, fullHeight, true);
+		
+		loadImages();
+		loadObjects(state);
+		FlxG.log.add("CheckPoint2");
+		// Load Tile Maps
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.TILE) continue;
+			var tileLayer:TiledTileLayer = cast layer;
+			FlxG.log.add("CheckPoint3");
+			var tileSheetName:String = tileLayer.properties.get("tileset");
+			
+			if (tileSheetName == null)
+				throw "'tileset' property not defined for the '" + tileLayer.name + "' layer. Please add the property to the layer.";
+				
+			var tileSet:TiledTileSet = null;
+			for (ts in tilesets)
+			{
+				if (ts.name == tileSheetName)
+				{
+					tileSet = ts;
+					break;
+				}
+			}
+			FlxG.log.add("CheckPoint4");
+			
+			if (tileSet == null)
+				throw "Tileset '" + tileSheetName + " not found. Did you misspell the 'tilesheet' property in " + tileLayer.name + "' layer?";
+				
+			var imagePath 		= new Path(tileSet.imageSource);
+			var processedPath 	= c_PATH_LEVEL_TILESHEETS + imagePath.file + "." + imagePath.ext;
+			
+			// could be a regular FlxTilemap if there are no animated tiles
+			var tilemap = new FlxTilemapExt();
+			tilemap.loadMapFromArray(tileLayer.tileArray, width, height, processedPath,
+				tileSet.tileWidth, tileSet.tileHeight, OFF, tileSet.firstGID, 1, 1);
+			
+			FlxG.log.add("CheckPoint5");
+			if (tileLayer.properties.contains("animated"))
+			{
+				var tileset = tilesets["level"];
+				var specialTiles:Map<Int, TiledTilePropertySet> = new Map();
+				for (tileProp in tileset.tileProps)
+				{
+					if (tileProp != null && tileProp.animationFrames.length > 0)
+					{
+						specialTiles[tileProp.tileID + tileset.firstGID] = tileProp;
+					}
+				}
+				var tileLayer:TiledTileLayer = cast layer;
+				tilemap.setSpecialTiles([
+					for (tile in tileLayer.tiles)
+						if (tile != null && specialTiles.exists(tile.tileID))
+							getAnimatedTile(specialTiles[tile.tileID], tileset)
+						else null
+				]);
+			}
+			
+			
+			
+			if (tileLayer.properties.contains("nocollide"))
+			{
+				backgroundLayer.add(tilemap);
+			}
+			else
+			{
+				if (collidableTileLayers == null)
+					collidableTileLayers = new Array<FlxTilemap>();
+				
+				foregroundTiles.add(tilemap);
+				foregroundObjects.add(tilemap);
+				BGObjects.add(tilemap);
+				collidableTileLayers.push(tilemap);
+			}
+		}
+		FlxG.log.add("CheckPointFinal?");
+	}
+
+	private function getAnimatedTile(props:TiledTilePropertySet, tileset:TiledTileSet):FlxTileSpecial
+	{
+		var special = new FlxTileSpecial(1, false, false, 0);
+		var n:Int = props.animationFrames.length;
+		var offset = Std.random(n);
+		special.addAnimation(
+			[for (i in 0...n) props.animationFrames[(i + offset) % n].tileID + tileset.firstGID],
+			(1000 / props.animationFrames[0].duration)
+		);
+		return special;
+	}
+	
+	public function loadObjects(state:PlayState)
+	{
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.OBJECT)
+				continue;
+			var objectLayer:TiledObjectLayer = cast layer;
+			
+			//collection of images layer
+			if (layer.name == "images")
+			{
+				for (o in objectLayer.objects)
+				{
+					loadImageObject(o);
+				}
+			}
+			
+			//objects layer
+			if (layer.name == "objects" || layer.name == "pickups")
+			{
+				for (o in objectLayer.objects)
+				{
+					loadObject(state, o, objectLayer, objectsLayer);
+				}
+			}
+		}
+	}
+	
+	private function loadImageObject(object:TiledObject)
+	{
+		var tilesImageCollection:TiledTileSet = this.getTileSet("imageCollection");
+		var tileImagesSource:TiledImageTile = tilesImageCollection.getImageSourceByGid(object.gid);
+		
+		//decorative sprites
+		var levelsDir:String = "assets/tiled/";
+		
+		var decoSprite:FlxSprite = new FlxSprite(0, 0, levelsDir + tileImagesSource.source);
+		if (decoSprite.width != object.width ||
+			decoSprite.height != object.height)
+		{
+			decoSprite.antialiasing = true;
+			decoSprite.setGraphicSize(object.width, object.height);
+		}
+		if (object.flippedHorizontally)
+		{
+			decoSprite.flipX = true;
+		}
+		if (object.flippedVertically)
+		{
+			decoSprite.flipY = true;
+		}
+		decoSprite.setPosition(object.x, object.y - decoSprite.height);
+		decoSprite.origin.set(0, decoSprite.height);
+		if (object.angle != 0)
+		{
+			decoSprite.angle = object.angle;
+			decoSprite.antialiasing = true;
+		}
+		
+		//Custom Properties
+		if (object.properties.contains("depth"))
+		{
+			var depth = Std.parseFloat( object.properties.get("depth"));
+			decoSprite.scrollFactor.set(depth,depth);
+		}
+		backgroundLayer.add(decoSprite);
+	}
+	
+	private function loadObject(state:PlayState, o:TiledObject, g:TiledObjectLayer, group:FlxGroup)
+	{
+		var x:Int = o.x;
+		var y:Int = o.y;
+		
+		// objects in tiled are aligned bottom-left (top-left in flixel)
+		if (o.gid != -1)
+			y -= g.map.getGidOwner(o.gid).tileHeight;
+		
+		switch (o.type.toLowerCase())
+		{
+			case "floor":
+				var floor = new FlxObject(x, y, o.width, o.height);
+				state.floor = floor;
+				
+			case "oob":
+				var oob = new FlxObject(x, y, o.width, o.height);
+				state._grpOOB.add(oob);
+				
+			case "pickup":
+				var pickup = new PickupSpot(x, y, o.properties.get("itemType"));
+				state._grpPickupSpots.add(pickup);
+			case "player_start":
+				state._player = new Player(x, y);
+				group.add(state._player);
+				FlxG.log.add("Player object added");
+			/*	
+			case "coin":
+				var tileset = g.map.getGidOwner(o.gid);
+				var coin = new FlxSprite(x, y, c_PATH_LEVEL_TILESHEETS + tileset.imageSource);
+				state.coins.add(coin);
+				
+			case "exit":
+				// Create the level exit
+				var exit = new FlxSprite(x, y);
+				exit.makeGraphic(32, 32, 0xff3f3f3f);
+				exit.exists = false;
+				state.exit = exit;
+				group.add(exit);
+			*/
+		}
+	}
+
+	public function loadImages()
+	{
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.IMAGE)
+				continue;
+
+			var image:TiledImageLayer = cast layer;
+			var sprite = new FlxSprite(image.x, image.y, c_PATH_LEVEL_TILESHEETS + image.imagePath);
+			imagesLayer.add(sprite);
+		}
+	}
+	
+	public function collideWithLevel(obj:Dynamic, ?notifyCallback:FlxObject->FlxObject->Void, ?processCallback:FlxObject->FlxObject->Bool):Bool
+	{
+		if (collidableTileLayers == null)
+			return false;
+		
+		for (map in collidableTileLayers)
+		{
+			// IMPORTANT: Always collide the map with objects, not the other way around.
+			//            This prevents odd collision errors (collision separation code off by 1 px).
+			if (FlxG.overlap(map, obj, notifyCallback, processCallback != null ? processCallback : FlxObject.separate))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
