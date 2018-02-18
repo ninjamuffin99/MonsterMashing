59cbb9a1b15a63c2733c03e148128fa371eb989e
Brandon <brandy@scarecrowarts.com>
2018-02-18 15:30:33 -0500

    mash

---
+++ source/MashState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxSubState;
+import flixel.addons.effects.chainable.FlxEffectSprite;
+import flixel.addons.effects.chainable.FlxWaveEffect;
+import flixel.group.FlxSpriteGroup;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.ui.FlxBar;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import openfl.geom.Matrix;
+import openfl.geom.Point;
+//import ui.Strings;
+//import ui.StringIDs;
+
+/**
+ * ...
+ * @author brandybuizel
+ */
+class MashState extends FlxState{
+
+}
