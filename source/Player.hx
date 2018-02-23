f2f321f09604339ae82ccb87c942d0444d515554
Cameron <cameron.taylor.ninja@gmail.com>
2018-02-22 21:02:20 -0800

    got a simple dash in

--- source/Player.hx
+++ source/Player.hx
@@ -15,6 +15,19 @@ class Player extends FlxSprite
 	private var speed:Float = 150;
 	
 	public var _up:Bool = false;
+	public var _down:Bool = false;
+	public var _left:Bool = false;
+	public var _right:Bool = false;
+	
+	public var _upP:Bool = false;
+	public var _downP:Bool = false;
+	public var _leftP:Bool = false;
+	public var _rightP:Bool = false;
+	
+	private var boostTimer:Float = 0.25;
+	private var boostLorR:String;
+	
+	
 	public var _upR:Bool = false;
 	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
@@ -28,7 +41,7 @@ class Player extends FlxSprite
 		animation.add("d", [6, 7, 8], frameRate);
 		animation.add("u", [9, 10, 11], frameRate);
 
-		drag.x = drag.y = 2800;
+		drag.x = drag.y = 1700;
 		
 		width = 13;
 		offset.x = 3;
@@ -41,28 +54,21 @@ class Player extends FlxSprite
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-
-		//rotation();
 		controls();
 	}
-
-	private function rotation():Void
-	{
-		var rads:Float = Math.atan2(FlxG.mouse.y - this.y, FlxG.mouse.x - this.x);
-		FlxG.watch.addQuick("Rads", Math.atan2(FlxG.mouse.y - this.y, FlxG.mouse.x - this.x));
-
-		var degs = rads * 180 / Math.PI;
-		FlxG.watch.addQuick("Degs/Angle", degs);
-		angle = degs + 90;
-	}
-
+	
 	//Movement controls
 	private function controls():Void
 	{
 		_up = FlxG.keys.anyPressed([UP, W]);
-		var _down = FlxG.keys.anyPressed([DOWN, S]);
-		var _left = FlxG.keys.anyPressed([LEFT, A]);
-		var _right = FlxG.keys.anyPressed([RIGHT, D]);
+		_down = FlxG.keys.anyPressed([DOWN, S]);
+		_left = FlxG.keys.anyPressed([LEFT, A]);
+		_right = FlxG.keys.anyPressed([RIGHT, D]);
+		
+		_upP = FlxG.keys.anyJustPressed([UP, W]);
+		_downP = FlxG.keys.anyJustPressed([DOWN, "S"]);
+		_leftP = FlxG.keys.anyJustPressed(["LEFT", "A"]);
+		_rightP = FlxG.keys.anyJustPressed(["RIGHT", "D"]);
 		
 		_upR = FlxG.keys.anyJustReleased([UP, W]);
 		
@@ -72,6 +78,7 @@ class Player extends FlxSprite
 			_left = _right = false;
 		
 		animation.play("u");
+		
 		
 		if (/*_up || _down || */_left || _right)
 		{
@@ -109,8 +116,12 @@ class Player extends FlxSprite
 				facing = FlxObject.RIGHT;
 				mA = 0;
 			}
+			
+			
+			
 			velocity.set(speed, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), mA);
+			
 			
 			/*
 			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE)
@@ -128,8 +139,6 @@ class Player extends FlxSprite
 				}
 			}
 			*/
-			
-			
 		}
 		else if (animation.curAnim != null)
 		{
@@ -137,5 +146,43 @@ class Player extends FlxSprite
 			animation.curAnim.pause();
 			//velocity.set(0,0);
 		}
+		
+		
+		
+		boostTimer -= FlxG.elapsed;
+		if (_leftP || _rightP)
+		{
+			
+			if (boostTimer <= 0)
+			{
+				boostTimer = 1;
+				FlxG.log.add("left pressed boost up");
+				
+				if (_leftP)
+					boostLorR = "left";
+				else
+					boostLorR = "right";
+				
+			}
+			else
+			{
+				var dash:Float = 850;
+				if (_leftP && boostLorR == "left")
+				{
+					velocity.x -= dash;
+				}
+				else if (_rightP && boostLorR == "right")
+				{
+					velocity.x += dash;
+				}
+				
+				boostTimer = 0;
+				
+				FlxG.log.add("left pressed speed up");
+				
+			}
+		}
+		
+		
 	}
 }
