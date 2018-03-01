a8a15179a230f0433ba2ca9e1b547d4b1e6347a7
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-01 00:06:01 -0800

    menu bs

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -4,6 +4,8 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
@@ -18,12 +20,15 @@ class MenuState extends FlxState
 		FlxG.save.bind("File");
 		Score.load();
 		
-		mTxt = new FlxText(0, 0, 0, "Press Z Play", 32);
+		mTxt = new FlxText(0, 0, 0, "MONSTER MASHING\nWIP TITLE SCREEN LMAOO\nPress Z Play", 32);
 		mTxt.screenCenter();
 		add(mTxt);
-		mScore = new FlxText(0, FlxG.height / 2 + 48, 0, "High Score: " + Score.score, 32);
+		mScore = new FlxText(0, FlxG.height / 2 + 70, 0, "High Score: " + Score.score, 32);
 		mScore.screenCenter(X);
 		add(mScore);
+		
+		FlxTween.tween(mTxt, {y: mTxt.y + 40}, 0.9, {type:FlxTween.PINGPONG, ease:FlxEase.quadInOut});
+		FlxTween.tween(mScore, {y: mScore.y + 40}, 0.9, {type:FlxTween.PINGPONG, ease:FlxEase.quadInOut});
 		
 		//add(hScore);
 		
