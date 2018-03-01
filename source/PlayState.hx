6fbe9204f6410a44d3bcbc4eb9919ff03a27660d
Cameron <cameron.taylor.ninja@gmail.com>
2018-02-28 18:44:46 -0800

    small fix shhhhh

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -314,7 +314,7 @@ class PlayState extends FlxState
 		}
 		
 		//loads new tilemap data to _map, from a .oel file
-		_map = new FlxOgmoLoader("assets/data/" + FlxG.random.int(1, 10) + ".oel");
+		_map = new FlxOgmoLoader("assets/data/" + FlxG.random.int(1, 7) + ".oel");
 		
 		//loads the _map data to the current tilemap(t), and has type shit too
 		t = _map.loadTilemap("assets/data/tile_temple.png", 16, 16, type);
