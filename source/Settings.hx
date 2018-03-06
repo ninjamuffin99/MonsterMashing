da883ef256dbebc39cc289729eddbc99cfe5975b
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-05 20:10:12 -0800

    getting a settings scren setup

---
+++ source/Settings.hx
@@ -0,0 +1,12 @@
+package;
+
+/**
+ * ...
+ * @author ninjaMuffin
+ */
+class Settings 
+{
+	public static var musicVol:Float = 1;
+	public static var soundVol:Float = 1;
+	public static var moanVol:Float = 1;
+}
