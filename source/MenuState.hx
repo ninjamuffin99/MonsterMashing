c466f7779b703a921c188f0304c0fc4fb472c877
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-07 23:26:52 -0800

    level and new logo in main menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -3,7 +3,10 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
@@ -22,11 +25,30 @@ class MenuState extends FlxState
 	private var mScore:FlxText;
 	private var credsTxt:FlxText;
 	
-	private var bg:FlxSprite;
-	
 	private var debugInfo:FlxText;
 	private var currentVersion:String = "v1.1.0";
 	
+	private var sprMonster:FlxSprite;
+	private var sprMashing:FlxSprite;
+	
+	//MAP SHIT
+	/**
+	 * The map data, loaded from Ogmo
+	 */
+	private var _map:FlxOgmoLoader;
+	
+	private var _mWalls:FlxTilemap;
+	private var _mFloors:FlxTilemap;
+	//The second group of tilemaps
+	private var _mWalls2:FlxTilemap;
+	private var _mFloors2:FlxTilemap;
+	
+	private var _mWalls3:FlxTilemap;
+	private var _mFloors3:FlxTilemap;
+	
+	private var _grpTilemaps:FlxTypedGroup<FlxTilemap>;
+	private var _grpWalls:FlxTypedGroup<FlxTilemap>;
+	private var speed:Float = 3;
 	
 	
 	override public function create():Void
@@ -36,13 +58,19 @@ class MenuState extends FlxState
 		
 		FlxG.camera.fade(FlxColor.BLACK, 0.8, true);
 		
-		bg = new FlxSprite().loadGraphic(AssetPaths.starting__PNG, false, 545, 201);
-		bg.setGraphicSize(Std.int(bg.width * 2));
-		bg.updateHitbox();
-		bg.screenCenter(X);
-		add(bg);
+		initTilemap();
 		
-		mTxt = new FlxText(0, 0, 0, "MONSTER MASHING\nPress Z to Play\nPress X to Shop", 32);
+		
+		sprMonster = new FlxSprite(0, 30).loadGraphic(AssetPaths.monster__png, false, 492, 166);
+		sprMonster.screenCenter(X);
+		add(sprMonster);
+		
+		sprMashing = new FlxSprite(0, 160).loadGraphic(AssetPaths.mashing__png, false, 319, 63);
+		sprMashing.screenCenter(X);
+		add(sprMashing);
+		
+		
+		mTxt = new FlxText(0, 0, 0, "Press Z to Play\nPress X to Shop", 32);
 		mTxt.screenCenter();
 		mTxt.y += 40;
 		add(mTxt);
@@ -84,6 +112,52 @@ class MenuState extends FlxState
 		
 		//new FlxTimer().start(1, finTim);
 		
+	}
+	
+	private function initTilemap():Void
+	{
+		_grpTilemaps = new FlxTypedGroup<FlxTilemap>();
+		add(_grpTilemaps);
+		
+		_grpWalls = new FlxTypedGroup<FlxTilemap>();
+		add(_grpWalls);
+		
+		//loads a new oel for the _map variable
+		_map = new FlxOgmoLoader("assets/data/start.oel");
+		
+		_mFloors = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Floor");
+		_grpTilemaps.add(_mFloors);
+		
+		_mWalls = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Walls");
+		_grpWalls.add(_mWalls);
+		
+		//loads a new oel to use, this time one with seamless tops and bottoms
+		_map = new FlxOgmoLoader("assets/data/1.oel");
+		
+		_mFloors2 = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Floor");
+		_mFloors2.y -= 16 * 36;
+		_grpTilemaps.add(_mFloors2);
+		
+		_mWalls2 = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Walls");
+		_mWalls2.y -= 16 * 36;
+		_grpWalls.add(_mWalls2);
+		
+		_mFloors3 = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Floor");
+		_mFloors3.y -= 16 * 36 * 2;
+		_grpTilemaps.add(_mFloors3);
+		
+		_mWalls3 = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, "Walls");
+		_mWalls3.y -= 16 * 36 * 2;
+		_grpWalls.add(_mWalls3);
+		
+		_grpTilemaps.forEach(scaleUp);
+		_grpWalls.forEach(scaleUp);
+		
+	}
+	
+	private function scaleUp(t:FlxTilemap):Void
+	{
+		t.scale.set(3, 3);
 	}
 	
 	private function finTim(t:FlxTimer):Void
@@ -134,6 +208,74 @@ class MenuState extends FlxState
 			
 		#end
 		
+		_grpTilemaps.forEach(checkTilemapPos);
+		_grpWalls.forEach(checkWallPos);
+		
 		super.update(elapsed);
 	}
+	
+	
+	private function checkTilemapPos(t:FlxTilemap):Void
+	{
+		updatePos(t, "Floor");
+	}
+	
+	private function checkWallPos(w:FlxTilemap):Void
+	{
+		updatePos(w, "Walls");
+	}
+	
+	/**
+	 * 
+	 * @param	t A FlxTilemap that'll get moved down the screen, and checked if it should be re-generated or shit
+	 * @param	type A string, with what the layer name is called in the oel/oep, mostly used
+	 * in the generateTilemap() function to keep _grpWalls and _grpTilemaps seperated. It should only be either "Walls" or "Floor"
+	 */
+	private function updatePos(t:FlxTilemap, type:String)
+	{
+		t.y += speed;
+		
+		// if the tilemap's y pos, is greater than the height(864) divided by 5(because of the zoom), 
+		//then it moves it 2 tilemap's up
+		if (t.y >= FlxG.height)
+		{
+			generateTilemap(t, type);
+		}
+	}
+	
+	private function generateTilemap(t:FlxTilemap, type:String):Void
+	{	
+		//basically, the tilemap actually needs to be removed entirely to be updated with a new _map/.oel file
+		//to keep the _grpWalls/_grpTilemaps simple, it also has splice set to true
+		//which also helps with the positioning shit i guess
+		if (type == "Walls")
+		{
+			_grpWalls.remove(t, true);
+		}
+		if (type == "Floor")
+		{
+			_grpTilemaps.remove(t, true);
+		}
+		
+		_map = new FlxOgmoLoader("assets/data/" + FlxG.random.int(1, 9) + ".oel");
+		
+		t = _map.loadTilemap("assets/data/tile_temple_0.png", 16, 16, type);
+		scaleUp(t);
+		
+		
+		//if the type is "Walls", it simply adds the tilemap back in
+		//if it's "Floor", then it also spawns some enemies, more info below
+		var tilesOffset:Float = 2;
+		if (type == "Walls")
+		{
+			t.y = _grpWalls.members[0].y - (t.height * tilesOffset);
+			_grpWalls.add(t);
+		}
+		if (type == "Floor")
+		{
+			t.y = _grpTilemaps.members[0].y - (t.height * tilesOffset);
+			_grpTilemaps.add(t);
+		}
+	}
+	
 }
