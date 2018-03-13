6a9e2a33583bb8dc1ec618da74627f7df0b34c94
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-13 15:32:58 -0700

    got preloader loading bar working

--- Preloader.hx
+++ Preloader.hx
@@ -1,7 +1,5 @@
 //SHOUTOUTS TO GAMEPOPPER FOR THE BALLIN TUTORIAL
 //https://gamepopper.co.uk/2014/08/26/haxeflixel-making-a-custom-preloader/
-
-
 package;
 
 import flixel.FlxG;
@@ -10,9 +8,9 @@ import flash.display.*;
 import flash.text.*;
 import flash.Lib;
 import flixel.text.FlxText;
-import io.newgrounds.NG;
 import lime.audio.FlashAudioContext;
 import openfl.display.Sprite;
+import openfl.display.BitmapData;
 import flash.text.Font;
 import flash.text.TextField;
 import flash.text.TextFormat;
@@ -22,16 +20,17 @@ import flash.text.TextFormat;
 class Preloader extends FlxBasePreloader 
 {
 
-	public function new(MinDisplayTime:Float=5, ?AllowedURLs:Array<String>) 
+	override public function new(MinDisplayTime:Float=4, ?AllowedURLs:Array<String>) 
 	{
 		super(MinDisplayTime, AllowedURLs);
 	}
 	
 	var logo:Sprite;
-	var bg:Sprite;
 	var text:TextField;
+	private var _buffer:Sprite;
+	private var _bmpBar:Bitmap;
 	
-	override function create():Void 
+	override private function create():Void 
 	{	
 		this._width = Lib.current.stage.stageWidth;
 		this._height = Lib.current.stage.stageHeight;
@@ -40,46 +39,39 @@ class Preloader extends FlxBasePreloader
 		
 		logo = new Sprite();
 		logo.addChild(new Bitmap(new LogoImage(0, 0))); //Sets the graphic of the sprite to a bitmap object, which uses our embedded bitmapData class
-		//logo.scaleX = 1.6;
-		//logo.scaleY = 2.8;
-		//logo.scaleX = logo.scaleY = ratio;
-		//logo.x = ((this._width) / 2) - ((logo.width) / 2);
-		//logo.y = (this._height / 2) - ((logo.height) / 2);
 		addChild(logo);
-		/*
-		var font:CustomFont = new CustomFont();
 		
-		Font.registerFont(CustomFont);
+		_buffer = new Sprite();
+		//_buffer.scaleX = _buffer.scaleY = 2;
+		addChild(_buffer);
 		
-		text = new TextField();
-		text.defaultTextFormat = new TextFormat(font.fontName, 24, 0xffffff, false, false, false, "", "", TextFormatAlign.CENTER);
-		text.embedFonts = true;
-		text.selectable = false;
-		text.multiline = false;
-		text.x = 0;
-		text.y = 5.2 * this._height / 6;
-		text.width = _width;
-		text.height = Std.int(32 * ratio);
-		text.text = "Loading";
-		addChild(text);
-		*/
+		_bmpBar = new Bitmap(new BitmapData(1, 14, false, 0x5f6aff));
+		_bmpBar.x = 4;
+		_bmpBar.y = _height - 17;
+		_buffer.addChild(_bmpBar);
 		
-		//finTim();
 		
 		super.create();
 	}
 	
-	
-	private function finTim():Void
+	override private function destroy():Void 
 	{
-		var newgrounds:NGio = new NGio(APIStuff.APIID, APIStuff.EncKey, this.stage);
+		if (_buffer != null)	
+		{
+			removeChild(_buffer);
+		}
+		_buffer = null;
+		_bmpBar = null;
+		logo = null;
+		
+		super.destroy();
 	}
 	
-	override function update(Percent:Float):Void
+	override public function update(Percent:Float):Void 
 	{
-		//text.text = "Loading " + Std.int(Percent * 100) + "%";
 		super.update(Percent);
 		
+		_bmpBar.scaleX = Percent * (_width - 8);
 	}
 	
 }
