9db58de8c520162342e976e38ac335062076333c
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-22 15:16:11 -0700

    more settings setup

--- source/SettingState.hx
+++ source/SettingState.hx
@@ -1,6 +1,7 @@
 package;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 
 /**
@@ -18,14 +19,28 @@ class SettingState extends FlxState
 	private var musicTxt:FlxText;
 	private var speedTxt:FlxText;
 	
+	private var settingsArray:Array<Dynamic> = [["Music Volume", "Sound Effect Volume", "Moan Volume", "Game Speed"], [musicVol, soundVol, moanVol, gameSpeed]];
+	
+	private var _selector:FlxSpriteGroup;
+	private var _selLeft:FlxText;
+	private var _selRight:FlxText;
+	
 	override public function create():Void 
 	{
-		musicTxt = new FlxText(0, 60, 0, "Music Volume", 32);
-		musicTxt.screenCenter(X);
-		add(musicTxt);
+		_selector = new FlxSpriteGroup();
+		add(_selector);
 		
-		speedTxt = new FlxText(100, 100, 0, "game speed: ", 32);
-		add(speedTxt);
+		_selLeft = new FlxText(0, 0, 0, "<", 32);
+		_selector.add(_selLeft);
+		
+		_selRight = new FlxText(100, 0, 0, ">", 32);
+		_selector.add(_selRight);
+		
+		for (i in 0...settingsArray[0].length)
+		{
+			var settingText:FlxText = new FlxText(32, (34 * i) + 100, 0, settingsArray[0][i], 32);
+			add(settingText);
+		}
 		
 		super.create();
 	}
@@ -37,6 +52,15 @@ class SettingState extends FlxState
 		speedTxt.text = "Game Speed: " + gameSpeed;
 		
 		if (FlxG.keys.justPressed.W)
+		{
+			_selector.y -= 64;
+		}
+		else if (FlxG.keys.justPressed.S)
+		{
+			_selector.y += 64;
+		}
+		
+		if (FlxG.keys.justPressed.ENTER)
 		{
 			FlxG.switchState(new MenuState());
 		}
