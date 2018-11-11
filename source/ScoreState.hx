a9d84bc2fa9465f8a017e008b2b5a6359e4a94c0
cameron taylor <cameron.taylor.ninja@gmail.com>
2018-11-10 20:20:56 -0800

    polish/fixins on the hall of shame scoreboard loading

--- source/ScoreState.hx
+++ source/ScoreState.hx
@@ -8,6 +8,7 @@ import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import io.newgrounds.NG;
+import io.newgrounds.objects.Score;
 
 /**
  * ...
@@ -53,59 +54,19 @@ class ScoreState extends FlxSubState
 	
 	private function checkScores():Void
 	{
-		
 		if (NGio.isLoggedIn && NGio.scoreboardsLoaded)
 		{
 			
 			NG.core.scoreBoards.get(8004).requestScores(20);
 			
-			var leaderBoardPlacement:Int = 1;
-			
-			
-			for (score in NG.core.scoreBoards.get(8004).scores)
+			if (NGio.scoreboardArray.length > 2)
+				namesPlacement(NGio.scoreboardArray)
+			else
 			{
-				var dev:Bool = false;
-				var isPlayer:Bool = false;
-				var userName:String = score.user.name;
-				
-				if (NG.core.user.name == score.user.name)
-				{
-					isPlayer = true;
-					
-					var shameMedal = NG.core.medals.get(54477);
-					if (!shameMedal.unlocked)
-						shameMedal.sendUnlock();
-					
-				}
-				
-				if (userName == "ninjamuffin99" || userName == "BrandyBuizel" || userName == "DIGIMIN")
-				{
-					dev = true;
-					userName += " (dev)";
-				}
-				
-				var text:String = Std.string(leaderBoardPlacement + ". " + userName + " - " + score.formatted_value);
-				
-				var name:FlxText = new FlxText(20, 32 + (34 * _grpText.members.length), FlxG.width - 20, text, 24);
-				_grpText.add(name);
-				
-				if (dev)
-				{
-					name.color = FlxColor.YELLOW;
-				}
-				
-				if (isPlayer)
-				{
-					name.color = FlxColor.RED;
-				}
-				
-				
-				leaderBoardPlacement += 1;
-				
-				trace('score loaded user:${score.user.name}, score:${score.formatted_value}');
-				
-				
+				namesPlacement(NG.core.scoreBoards.get(8004).scores);
+				NGio.scoreboardArray = NG.core.scoreBoards.get(8004).scores;
 			}
+			
 			
 			bountyTxt = new FlxText(0, FlxG.height - 112, 0, "\nBOUNTIES\nn/a", 16);
 			bountyTxt.screenCenter(X);
@@ -124,9 +85,58 @@ class ScoreState extends FlxSubState
 		}
 	}
 	
+	private function namesPlacement(scoreArray:Array<Score>):Void
+	{
+		var leaderBoardPlacement:Int = 1;
+		
+		for (score in scoreArray)
+		{
+			var dev:Bool = false;
+			var isPlayer:Bool = false;
+			var userName:String = score.user.name;
+			
+			if (NG.core.user.name == userName)
+			{
+				isPlayer = true;
+				
+				var shameMedal = NG.core.medals.get(54477);
+				if (!shameMedal.unlocked)
+					shameMedal.sendUnlock();
+				
+			}
+			
+			if (userName == "ninjamuffin99" || userName == "BrandyBuizel" || userName == "DIGIMIN")
+			{
+				dev = true;
+				userName += " (dev)";
+			}
+			
+			var text:String = Std.string(leaderBoardPlacement + ". " + userName + " - " + score.formatted_value);
+			
+			var name:FlxText = new FlxText(20, 32 + (34 * _grpText.members.length), FlxG.width - 20, text, 24);
+			_grpText.add(name);
+			
+			if (dev)
+			{
+				name.color = FlxColor.YELLOW;
+			}
+			
+			if (isPlayer)
+			{
+				name.color = FlxColor.RED;
+			}
+			
+			
+			leaderBoardPlacement += 1;
+			
+			
+		}
+		
+	}
+	
 	override public function update(elapsed:Float):Void 
 	{
-		if (scoreboardInitialized = false && NGio.scoreboardsLoaded)
+		if (!scoreboardInitialized && NGio.scoreboardsLoaded)
 		{
 			checkScores();
 		}
@@ -134,6 +144,14 @@ class ScoreState extends FlxSubState
 		if (!NGio.scoreboardsLoaded && NGio.isLoggedIn)
 		{
 			hallOfShame.text = "HALL OF SHAME\n\nScoreboards loading...\n\n";
+		}
+		
+		if (NGio.scoreboardsLoaded && NGio.isLoggedIn)
+		{
+			if (FlxG.keys.justPressed.R)
+			{
+				namesPlacement(NG.core.scoreBoards.get(8004).scores);
+			}
 		}
 		
 		if (FlxG.keys.justPressed.ANY)
