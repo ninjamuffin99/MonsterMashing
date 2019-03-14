68f74e21275bb19de93e516fafe0c010f6960eee
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-03-13 18:30:22 -0700

    made build script look cool

--- art/build scripts/buildUpload-Android-Itch.bat
+++ art/build scripts/buildUpload-Android-Itch.bat
@@ -1,5 +1,8 @@
+@echo off
+color 0d
 cd ..
 cd ..
+@echo on
 lime build android -release
 butler push ./export/release/android/bin/bin/MonsterMashing-debug.apk ninja-muffin24/monster-mashing:android
 butler status ninja-muffin24/monster-mashing:android
