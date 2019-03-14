21cd9e0a4f3cc859b450b74f8d888c6cb10f5c39
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-03-13 18:30:22 -0700

    made build script look cool

--- art/build scripts/buildUpload-Windows-Itch.bat
+++ art/build scripts/buildUpload-Windows-Itch.bat
@@ -1,5 +1,8 @@
+@echo off
+color 0d
 cd ..
 cd ..
+@echo on
 lime build windows -release
 butler push ./export/release/windows/bin ninja-muffin24/monster-mashing:win
 butler status ninja-muffin24/monster-mashing:win
